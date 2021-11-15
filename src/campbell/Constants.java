f8f021102f88e469e19fe0bd2a377f2149c4c7d9
evancampbell <evan.troutt@inca.digital>
2021-11-15 11:41:05 -0500

    Add files via upload

---
+++ src/campbell/Constants.java
@@ -0,0 +1,5 @@
+
+public final class Constants {
+    public static final String PROJECT_ROOT = "/home/evan/eclipse-workspace/Test";
+    public static final String CAPTURES_DIR = "res/captures/";
+}
