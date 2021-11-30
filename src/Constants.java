2df1563278ec6d089b4a5a8e29a9cdb00ed1d5ab
evancampbell <evan.troutt@inca.digital>
2021-11-29 19:25:17 -0500

    Delete src directory

--- src/Constants.java
+++
@@ -1,5 +0,0 @@
-
-public final class Constants {
-    public static final String PROJECT_ROOT = "/home/evan/eclipse-workspace/Test";
-    public static final String CAPTURES_DIR = "res/captures/";
-}
