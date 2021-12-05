69bee8e6c537817fae94b3d792c93bd899c9275b
evancampbell <evan.troutt@inca.digital>
2021-12-05 16:17:16 -0500

    Update SSUserDetailsService.java

--- src/main/service/SSUserDetailsService.java
+++ src/main/service/SSUserDetailsService.java
@@ -2,10 +2,13 @@ package main.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import main.db.Site;
 import main.db.SiteRepository;
 import main.db.User;
 import main.db.UserRepository;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -26,9 +29,33 @@ public class SSUserDetailsService implements UserDetailsService {
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
+    @Autowired
+    private Logger logger;
+
     public User findUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
+
+    public void deleteByEmail(String email) {
+        User user = userRepository.findByEmail(email);
+        Set<Site> subscribedSites = userRepository.findByEmail(email).getSites();
+        for (Site s : subscribedSites) {
+            Set<User> users = s.getUsers();
+            for (User u : users) {
+                if (u.getEmail().equals(email)) {
+                    users.remove(u);
+                    s.setUsers(users);
+                }
+            }
+
+            //System.out.println("\nUser to delete: " + user.getId());
+            //System.out.println("Result of removal: " + b);
+            siteRepository.save(s);
+        }
+        userRepository.deleteByEmail(email);
+    }
+
+    public List<User> findAll() { return userRepository.findAll(); }
 
     public void saveUser(User user, String role) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
