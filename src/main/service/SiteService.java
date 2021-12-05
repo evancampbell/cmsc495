b2bbac17df74c4b9bb76beb717be53db77540c9c
evancampbell <evan.troutt@inca.digital>
2021-12-05 16:17:02 -0500

    Update SiteService.java

--- src/main/service/SiteService.java
+++ src/main/service/SiteService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -27,11 +28,16 @@ public class SiteService {
     public List<Site> findAll() { return siteRepository.findAll(); }
 
     public void saveSite(Site site, User user) {
+        if (siteRepository.findByUrl(site.getUrl()) != null) {
+            site = siteRepository.findByUrl(site.getUrl());
+        }
         Set<User> users = site.getUsers();
         if (users == null) {
             users = new HashSet<User>();
         }
-        users.add(user);
+        if (!users.contains(user)) {
+            users.add(user);
+        }
         site.setUsers(users);
         siteRepository.save(site);
 
@@ -39,7 +45,36 @@ public class SiteService {
         if (sites == null) {
             sites = new HashSet<Site>();
         }
-        sites.add(site);
+        boolean add = true;
+        for (Site s : sites) {
+            if (s.getUrl().equals(site.getUrl())) {
+                add = false;
+            }
+        }
+        if (add) sites.add(site);
+
+        user.setSites(sites);
+        userRepository.save(user);
+    }
+
+    public void removeUser(String url, User user) {
+        Site site = siteRepository.findByUrl(url);
+        Set<User> users = site.getUsers();
+        for (Iterator<User> it = users.iterator(); it.hasNext();) {
+            User u = it.next();
+            if (u.getEmail().equals(user.getEmail())) {
+                it.remove();
+            }
+        }
+        site.setUsers(users);
+        siteRepository.save(site);
+        Set<Site> sites = user.getSites();
+        for (Iterator<Site> it = sites.iterator(); it.hasNext();) {
+            Site s = it.next();
+            if (url.equals(s.getUrl())) {
+                it.remove();
+            }
+        }
         user.setSites(sites);
         userRepository.save(user);
     }
