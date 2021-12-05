385d8adedff1c241ee6322140abcf372a48cf944
evancampbell <evan.troutt@inca.digital>
2021-12-05 16:20:31 -0500

    Update UserRepository.java

--- src/main/db/UserRepository.java
+++ src/main/db/UserRepository.java
@@ -1,8 +1,11 @@
 package main.db;
 
-import main.db.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface UserRepository extends MongoRepository<User, String> {
     User findByEmail(String email);
+
+    @Query(value="{'email' : ?0}", delete=true)
+    public void deleteByEmail(String email);
 }
