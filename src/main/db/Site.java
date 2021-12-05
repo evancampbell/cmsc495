cd5b937922b770b470299f79384eed15f77d2e37
evancampbell <evan.troutt@inca.digital>
2021-12-05 16:19:52 -0500

    Update Site.java

--- src/main/db/Site.java
+++ src/main/db/Site.java
@@ -1,12 +1,10 @@
 package main.db;
 
-import main.db.User;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
 import java.util.Set;
 
 @Document(collection = "site")
